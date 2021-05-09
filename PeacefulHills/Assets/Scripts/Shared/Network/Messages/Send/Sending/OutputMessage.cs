4dc2e239839228dc2b57a77f8acfe792ee12e0a2
Daniel <helporme.dev@gmail.com>
2021-05-09 22:39:55 +0300

    Create the output message a priority for sending.

---
+++ PeacefulHills/Assets/Scripts/Shared/Network/Messages/Send/Sending/OutputMessage.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+
+namespace PeacefulHills.Network.Messages
+{
+    // 32 is max window size of the reliable pipeline
+    [InternalBufferCapacity(32)]
+    public struct OutputMessage : IBufferElementData
+    {
+        public unsafe byte* Bytes;
+    }
+}
