cbcdc77410aab792b29c1279b0e88613f398026e
Daniel <helporme.dev@gmail.com>
2021-05-09 22:30:08 +0300

    Create MessagesSendingInitializationSystem.cs for dependency singleton

---
+++ PeacefulHills/Assets/Scripts/Shared/Network/Messages/Send/Sending/MessagesSendingInitializationSystem.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+
+namespace PeacefulHills.Network.Messages
+{
+    [UpdateInGroup(typeof(NetworkInitializationGroup))]
+    public class MessagesSendingInitializationSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            SetSingleton(new MessagesSendingDependency());
+        }
+    }
+}
