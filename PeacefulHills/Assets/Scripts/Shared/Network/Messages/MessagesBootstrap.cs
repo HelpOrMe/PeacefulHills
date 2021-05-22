ba0f991ab3091f6c99a773e3368a9c2bf02221c6
Daniel <helporme.dev@gmail.com>
2021-05-22 03:59:52 +0300

    Add messages bootstrap

---
+++ PeacefulHills/Assets/Scripts/Shared/Network/Messages/MessagesBootstrap.cs
@@ -0,0 +1,16 @@
+﻿using PeacefulHills.Bootstrap;
+using PeacefulHills.ECS;
+using Unity.Entities;
+using UnityEngine;
+
+namespace PeacefulHills.Network.Messages
+{
+    [BootstrapWorld(typeof(NetworkWorldBootstrap))]
+    public class MessagesNetworkBootstrapPart : BootstrapWorldPart
+    {
+        public override void Initialize(World world)
+        {
+            world.SetExtension<IMessagesRegistry>(new MessagesRegistry());
+        }
+    }
+}
