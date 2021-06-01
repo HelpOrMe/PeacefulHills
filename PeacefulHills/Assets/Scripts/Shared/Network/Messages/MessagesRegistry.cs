3bd973c65dd76921d2983429c4f158a112d1e4b4
Daniel <helporme.dev@gmail.com>
2021-06-02 00:11:35 +0300

    Fix registry constructor

--- PeacefulHills/Assets/Scripts/Shared/Network/Messages/MessagesRegistry.cs
+++ PeacefulHills/Assets/Scripts/Shared/Network/Messages/MessagesRegistry.cs
@@ -6,15 +6,22 @@ namespace PeacefulHills.Network.Messages
     public class MessagesRegistry : IMessagesRegistry
     {
         private NativeHashMap<uint, MessageInfo> _messagesById;
-        private NativeHashMap<ulong, uint> _messageIdsByStableHash;
+        private NativeHashMap<ulong, uint> _messageIdsByStableHash; 
         
         private uint _lastMessageId;
+
+        public MessagesRegistry()
+        {
+            _messageIdsByStableHash = new NativeHashMap<ulong, uint>(1, Allocator.Persistent);
+            _messagesById = new NativeHashMap<uint, MessageInfo>(1, Allocator.Persistent);
+        }
 
         public uint Register<TMessage>() where TMessage : IMessage
         {
             uint id = _lastMessageId++;
             TypeManager.TypeInfo typeInfo = TypeManager.GetTypeInfo<TMessage>();
             _messagesById[_lastMessageId] = new MessageInfo(typeInfo, _lastMessageId);
+            _messageIdsByStableHash[typeInfo.StableTypeHash] = id;
             return id;
         }
 
