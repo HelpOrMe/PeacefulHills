90f68c8f89b33fb2df02e63b392434f07c31128f
Daniel <helporme.dev@gmail.com>
2021-08-04 12:39:02 +0300

    Add messages helper

---
+++ PeacefulHills/Assets/Scripts/Shared/Network/Messages/Serialization/NetworkMessagesSerialization.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+
+namespace PeacefulHills.Network.Messages
+{
+    public static partial class NetworkMessages
+    {
+        public static Entity Receive<TMessage>(MessageDeserializerContext context, TMessage message)
+            where TMessage : unmanaged, IMessage, IComponentData
+        {
+            Entity entity = Create(context.CommandBuffer, context.SortKey, context.Connection, message);
+            context.CommandBuffer.AddComponent<MessageReceiveRequest>(context.SortKey, entity);
+            return entity;
+        }
+    }
+}
